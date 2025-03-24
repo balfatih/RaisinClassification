4339f053d4b69edd0fbb787823dc1d6876b725b9
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-03-24 16:30:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🚧 Machine Learning Application for Raisin Classification')
 
 st.info("This application was developed for Raisin Classification. ")
 
-with st.expander('Raisin Dataset')
+with st.expander('Raisin Dataset'):
   st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/balfatih/RaisinClassification/refs/heads/main/Dataset/Raisin_Dataset.csv')
   df
