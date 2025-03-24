3e6273f3ee802cebcdc79ca468c307da992d2a60
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-03-24 16:28:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 
 import streamlit as st 
+import pandas as pd
 
 st.title('🚧 Machine Learning Application for Raisin Classification')
 
 st.info("This application was developed for Raisin Classification. ")
+
+df = pd.read_csv('https://raw.githubusercontent.com/balfatih/RaisinClassification/refs/heads/main/Dataset/Raisin_Dataset.csv')
+df
